e826de8b3aa245d5d7b2b480067d37472a96a0fb
Jacob Eliat-Eliat <jacobeliat@gmail.com>
2018-02-24 17:35:22 +0100

    trying to make cornerstone work

--- registrator/regtor/models.py
+++ registrator/regtor/models.py
@@ -2,13 +2,19 @@ from django.db import models
 
 # Create your models here.
 
-class coolness(models.Model):
-    identifier = models.IntField()
+class Coolness(models.Model):
+    identifier = models.IntegerField()
     name = models.CharField(max_length=42)
-
     class Meta:
         verbose_name = "test"
-        ordering = ['id']
+        ordering = ['identifier']
 
     def __str__(self):
-        return self.identifier
+        return str(self.identifier)
+
+class Member(models.Model):
+    name = models.CharField(max_length=42)
+    group = models.ForeignKey('Coolness', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
